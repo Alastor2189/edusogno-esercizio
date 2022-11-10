79a4d2599c87b3fc72cc318c6262be8d4cd66dea
Alastor2189 <g.lucafarci89@gmail.com>
2022-11-10 16:24:32 +0100

    completamento file di User

--- user.php
+++ user.php
@@ -17,7 +17,7 @@ class User {
                 return false;
             else {
                 $password = @sha1($password);
-                $query = @mysql_query("SELECT id FROM `utenti` WHERE `nome` = '$name' OR `email` = '$email'") or die('Errore: ' . mysql_error());
+                $query = @mysql_query("SELECT id FROM utenti WHERE nome = '$name' OR email = '$email'") or die('Errore: ' . mysql_error());
                 $count = @mysql_num_rows($query); 
             if ($count == 0)    {
                 $result = @mysql_query("INSERT INTO utenti(nome, password, surname, email) VALUES ('$name', '$password','$surname','$email')") or die('Errore: ' .mysql_error());
@@ -29,10 +29,44 @@ class User {
 
     }
 
-    public function autentication({
-
+    public function login($emailUser, $password) 
+    {
+    $password = @sha1($password);
+    $query = @mysql_query("SELECT id FROM utenti WHERE (email = '$emailUser' OR nome='$emailUser') AND password = '$password'") or die('Errore: ' . mysql_error());
+    $count = @mysql_num_rows($query);
+    if ($count == 1) {
+        $result = @mysql_fetch_object($query);
+        $_SESSION['login'] = true;
+        $_SESSION['id'] = $risultato->id;
+        return true;
+        }else{
+        return false;
     }
+    }
+
+    public function userLogged($id) 
+        {
+        $query = @mysql_query("SELECT nome FROM utenti WHERE id = '$id'") or die('Errore: ' . mysql_error());
+        $risultato = @mysql_fetch_object($query);
+        echo $risultato->nome;
+        }
+
+    public function verifySession() 
+        {
+          if(isset($_SESSION['login']))
+          {
+            return $_SESSION['login'];
+          }else{
+            return false;
+          }
+        }
+    public function esc()
+        {
+          $_SESSION['login'] = FALSE;
+          @session_destroy();
+          }
 }
+
 
 
 ?>
